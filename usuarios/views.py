805e23841da68f4446f5b3986817c420ff097161
devid <nnick4673@gmail.com>
2022-12-14 19:57:09 -0300

    Cadastro de usuários implementado

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
 
 def login(request):
 	return HttpResponse('login')
 
 def cadastro(request):
+	if request.method == 'POST':
+		nome = request.POST.get('nome')
+		email = request.POST.get('email')
+		senha = request.POST.get('senha')
+
+		try:
+			validate_email(email)
+		except:
+			return redirect('/usuario/cadastro/')
+		else:
+			if len(nome.replace(' ', '')) == 0:
+				return redirect('/usuario/cadastro/')
+			u = User(username = nome, password = senha)
+			try:
+				validate_password(senha, user = u)
+			except:
+				return redirect('/usuario/cadastro/')
+
+		usuario = User.objects.filter(username = email)
+
+		if not usuario:
+			usuario = User.objects.create_user(username = email, first_name = nome, password = senha)
+		
+		return redirect('/usuario/login/')
+
 	return render(request, 'cadastro.html')
