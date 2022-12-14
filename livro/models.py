917ac189c534f4dcaaba703f3c7e8804878a806d
devid <nnick4673@gmail.com>
2022-12-14 11:11:19 -0300

    Data de cadastro e de empréstimos setadas automaticamente

--- livro/models.py
+++ livro/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from datetime import date
-
 
 class Livros(models.Model):
 	nome = models.CharField(max_length = 100)
 	autor = models.CharField(max_length = 30)
 	co_autor = models.CharField(max_length = 30, blank = True, null = True)
-	data_cadastro = models.DateField(default = date.today())
+	data_cadastro = models.DateField(auto_now_add = True)
 	emprestado = models.BooleanField(default = False)
 
 	def __str__(self):
@@ -18,7 +16,7 @@ class Livros(models.Model):
 class Emprestimos(models.Model):
 	nome_emprestado = models.CharField(max_length = 30)
 	livro_emprestado = models.ForeignKey(Livros, on_delete = models.CASCADE)
-	data_emprestimo = models.DateField(default = date.today())
+	data_emprestimo = models.DateField(auto_now_add = True)
 	data_devolucao = models.DateField(blank = True, null = True)
 	duracao_emprestimo = models.CharField(max_length = 30, null = True)
 
