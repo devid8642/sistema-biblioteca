c3b4c172d5e9f273973398e311cd0642dd166092
devid <nnick4673@gmail.com>
2022-12-13 22:03:32 -0300

    Criada estrutura básica do banco de dados para a app livros

--- livro/admin.py
+++ livro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Livros, Emprestimos
 
 # Register your models here.
+
+admin.site.register(Livros)
+admin.site.register(Emprestimos)
--- livro/models.py
+++ livro/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Livros(models.Model):
+	nome = models.CharField(max_length = 100)
+	autor = models.CharField(max_length = 30)
+	co_autor = models.CharField(max_length = 30, blank = True, null = True)
+	data_cadastro = models.DateField(default = date.today())
+	emprestado = models.BooleanField(default = False)
+
+
+	def __str__(self):
+		return self.nome
+
+
+class Emprestimos(models.Model):
+	nome_emprestado = models.CharField(max_length = 30)
+	livro_emprestado = models.ForeignKey(Livros, on_delete = models.CASCADE)
+	data_emprestimo = models.DateField()
+	data_devolucao = models.DateField(null = True)
+
+	def __str__(self):
+		return self.nome_emprestado
