4cf07e8268c8bd6e0b55ce71266d84efe9242346
devid <nnick4673@gmail.com>
2022-12-14 19:16:37 -0300

    Algoritmo de hash argon2 configurado

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -106,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
