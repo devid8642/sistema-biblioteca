2529a6c018b0c4a21626073a5b069c45213466e2
devid <nnick4673@gmail.com>
2022-12-15 11:11:00 -0300

    Categorias para livros adicionada ao sistema

--- livro/admin.py
+++ livro/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Livros, Emprestimos
+from .models import Livros, Emprestimos, Categorias
 
 # Register your models here.
 
 admin.site.register(Livros)
 admin.site.register(Emprestimos)
+admin.site.register(Categorias)
--- livro/models.py
+++ livro/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+class Categorias(models.Model):
+	nome = models.CharField(max_length = 30)
+	descricao = models.TextField(blank = True, null = True)
+
+	def __str__(self):
+		return self.nome
+
+	class Meta:
+		verbose_name = 'Categoria'
+
 class Livros(models.Model):
 	nome = models.CharField(max_length = 100)
 	autor = models.CharField(max_length = 30)
 	co_autor = models.CharField(max_length = 30, blank = True, null = True)
 	data_cadastro = models.DateField(auto_now_add = True)
 	emprestado = models.BooleanField(default = False)
+	categoria = models.ForeignKey(Categorias, on_delete = models.SET_NULL, null = True)
 
 	def __str__(self):
 		return self.nome
