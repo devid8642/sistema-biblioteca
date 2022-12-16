e06a54f4cf7a71cd88ede8bc1b1f00ed7a0abba9
devid <nnick4673@gmail.com>
2022-12-16 16:54:01 -0300

    Feat: acessar os dados de cada livro

--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Livros, Emprestimos
+from .models import Livros, Emprestimos, Categorias
 from datetime import date
 
 # Create your views here.
@@ -16,5 +15,50 @@ def home(request):
 		}
 
 		return render(request, 'home.html', context)
+	else:
+		return redirect('/usuario/login/?status=4')
+
+def livro(request, id):
+	usuario = request.user
+
+	if usuario.is_authenticated:
+		try:
+			livro = Livros.objects.get(id = id)
+		except:
+			return redirect('/')
+		if request.method == 'POST':
+			nome_livro = request.POST.get('nome_livro')
+			autor_livro = request.POST.get('autor_livro')
+			co_autor = request.POST.get('co_autor')
+			categoria = nome = request.POST.get('categoria')
+
+			if len(nome_livro) > 250:
+				return redirect(f'/livro/{livro.id}/?status=1')
+			elif len(autor_livro) > 250 or len(co_autor) > 250:
+				return redirect(f'/livro/{livro.id}/?status=2')
+			elif len(nome_livro) == 0 or len(autor_livro) == 0:
+				return redirect(f'/livro/{livro.id}/?status=3')
+
+			try:
+				categoria = Categorias.objects.get(nome = categoria)
+			except:
+				return redirect(f'/livro/{livro.id}')
+
+			livro.nome = nome_livro
+			livro.autor = autor_livro
+			livro.co_autor = co_autor
+			livro.categoria = categoria
+
+			livro.save(update_fields = ['nome', 'autor', 'co_autor', 'categoria'])
+
+			return redirect('/')
+		status = request.GET.get('status')
+		categorias = Categorias.objects.all()
+		context = {
+			'livro': livro,
+			'categorias': categorias,
+			'status': status
+		}
+		return render(request, 'livro.html', context)
 	else:
 		return redirect('/usuario/login/?status=4')
