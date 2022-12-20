f20bf0e7d15d0564f08aff098d84275b674e8fb5
devid <nnick4673@gmail.com>
2022-12-20 17:48:40 -0300

    App configurada para usar o DB MySQL

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
