f0b8527c42c318c5711cc232fdbf4c26806210f1
devid <nnick4673@gmail.com>
2022-12-26 12:17:31 -0300

    Adicionado suporte a cache com o memcached

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -120,6 +120,12 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.ScryptPasswordHasher',
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
